449282edf1a53e99ad3f56e7ffd648b77a1f2e22
Soumya Rout <soumyarout80@gmail.com>
2019-04-16 22:21:49 +0530

    Initial commit

---
+++ sample-application/flaskblog.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Soumya Ranjan Rout',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 16, 2019'
+    },
+    {
+        'author': 'Sonali Mishra',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 16, 2019'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
